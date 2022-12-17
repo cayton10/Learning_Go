f40fbd1390fa62d9407b451ccc0a56659c6292a0
Obi-Wan <cayton10@live.marshall.edu>
2022-12-16 23:42:43 -0500

    These little things help us learn

--- web_service/main.go
+++ web_service/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World"))
+		names := r.URL.Query()["name"]
+		var name string
+		if len(names) == 1 {
+			name = names[0]
+		}
+
+		m := map[string]string{"name": name}
+
+		// Create JSON Encoder to serialize object
+		enc := json.NewEncoder(w)
+		enc.Encode(m);
 	})
 
 	err := http.ListenAndServe(":3000", nil)
